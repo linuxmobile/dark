fd045fb73822de739d0b63e7ad5a39fd4732f171
Braian A. Diez <bdiez19@gmail.com>
2024-10-12 14:14:02 -0300

    style: improved responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="grid grid-cols-12 grid-rows-6 gap-6 pb-6 mx-auto select-none">
+  <main class="grid grid-cols-1 lg:grid-cols-12 grid-rows-6 gap-6 pb-6 mx-auto select-none">
     <Tiles
       v-for="(color, index) in colors"
       :key="index"
@@ -37,7 +37,7 @@ const colors = [
 		name: "Rich Black",
 		hex: "#0A0A0A",
 		rgb: "rgb(10, 10, 10)",
-		class: "col-span-5",
+		class: "col-span-6 md:col-span-5",
 	},
 	{
 		name: "Coffee Bean",
@@ -49,19 +49,19 @@ const colors = [
 		name: "Dark Gray",
 		hex: "#212427",
 		rgb: "rgb(33, 36, 39)",
-		class: "col-span-4",
+		class: "col-span-3 md:col-span-4",
 	},
 	{
 		name: "Oil Black",
 		hex: "#0C0C0C",
 		rgb: "rgb(12, 12, 12)",
-		class: "col-span-3",
+		class: "col-span-6 md:col-span-4 xl:col-span-3",
 	},
 	{
 		name: "Obsidian",
 		hex: "#0B1215",
 		rgb: "rgb(11, 18, 21)",
-		class: "col-span-3",
+		class: "col-span-3 md:col-span-5 xl:col-span-3",
 	},
 	{
 		name: "Ebony",
@@ -73,31 +73,31 @@ const colors = [
 		name: "Black Chocolate",
 		hex: "#100D08",
 		rgb: "rgb(16, 13, 8)",
-		class: "col-span-3",
+		class: "col-span-6 md:col-span-5 xl:col-span-3",
 	},
 	{
 		name: "Gunmetal",
 		hex: "#1D1F21",
 		rgb: "rgb(29, 31, 33)",
-		class: "col-span-4",
+		class: "col-span-3 xl:col-span-4",
 	},
 	{
 		name: "Smoky Black",
 		hex: "#101720",
 		rgb: "rgb(16, 23, 32)",
-		class: "col-span-3",
+		class: "col-span-3 md:col-span-4 xl:col-span-3",
 	},
 	{
 		name: "Oxford Blue",
 		hex: "#212A37",
 		rgb: "rgb(33, 42, 55)",
-		class: "col-span-5",
+		class: "col-span-6 md:col-span-4 xl:col-span-5",
 	},
 	{
 		name: "Eerie Black",
 		hex: "#232023",
 		rgb: "rgb(35, 32, 35)",
-		class: "col-span-5",
+		class: "col-span-3 md:col-span-5",
 	},
 	{
 		name: "Jet Black",
@@ -109,38 +109,38 @@ const colors = [
 		name: "Iridium",
 		hex: "#181818",
 		rgb: "rgb(24, 24, 24)",
-		class: "col-span-4",
+		class: "col-span-6 md:col-span-5 xl:col-span-4",
 	},
 	{
 		name: "Arsenic",
 		hex: "#11181C",
 		rgb: "rgb(17, 24, 28)",
-		class: "col-span-4",
+		class: "col-span-3 xl:col-span-4",
 	},
 	{
 		name: "Charleston Green",
 		hex: "#212124",
 		rgb: "rgb(33, 33, 36)",
-		class: "col-span-3",
+		class: "col-span-3 md:col-span-4 xl:col-span-3",
 	},
 	{
 		name: "Dark Gunmetal",
 		hex: "#222428",
 		rgb: "rgb(34, 36, 40)",
-		class: "col-span-5",
+		class: "col-span-6 md:col-span-3 xl:col-span-5",
 	},
 	{ name: "Jet", hex: "#2A2A2A", rgb: "rgb(42, 42, 42)", class: "col-span-3" },
 	{
 		name: "Black Olive",
 		hex: "#242526",
 		rgb: "rgb(36, 37, 38)",
-		class: "col-span-5",
+		class: "col-span-3 xl:col-span-5",
 	},
 	{
 		name: "Midnight Blue",
 		hex: "#212121",
 		rgb: "rgb(33, 33, 33)",
-		class: "col-span-4",
+		class: "col-span-6 md:col-span-3 xl:col-span-4",
 	},
 ];
 
